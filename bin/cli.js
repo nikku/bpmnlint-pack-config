5000e777fc0257db8fe97ae0070160a58613ea16
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-29 23:00:05 +0200

    deps: replace meow with mri

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,37 @@
 #!/usr/bin/env node
 
-const meow = require('meow');
+const mri = require('mri');
 
 const { writeFileSync } = require('fs');
 
 const { packConfig } = require('..');
 
-const cli = meow(
-  `
+const options = mri(process.argv.slice(2), {
+
+  alias: {
+    c: 'config',
+    o: 'outfile',
+    t: 'target'
+  },
+
+  default: {
+    config: '.bpmnlintrc',
+    target: 'cjs',
+  },
+
+  string: [
+    'outfile',
+    'target-name'
+  ],
+
+  boolean: [
+    'help'
+  ]
+});
+
+
+if (options.help) {
+  console.log(`
   Usage
     $ bpmnlint-pack-config [-c ...] [-o outfile] [-t es|cjs|umd]
 
@@ -20,37 +44,23 @@ const cli = meow(
   Examples
     $ bpmnlint-pack-config -t es > packed-config.js
     $ bpmnlint-pack-config -c conf/bpmnlint.json -o conf/bpmnlint.json.js
+`);
 
-`,
-  {
-    flags: {
-      config: {
-        type: 'string',
-        alias: 'c',
-        default: '.bpmnlintrc'
-      },
-      outfile: {
-        type: 'string',
-        alias: 'o'
-      },
-      target: {
-        type: 'string',
-        alias: 't',
-        default: 'cjs'
-      },
-      targetName: {
-        type: 'string'
-      }
-    }
-  }
-);
+  process.exit(0);
+}
+
+if (!options.config) {
+  console.error('missing <config>');
+
+  process.exit(1);
+}
 
 async function run(options) {
   const {
     config,
     outfile,
     target,
-    targetName
+    'target-name': targetName
   } = options;
 
   const { code } = await packConfig(config, target, targetName);
@@ -63,4 +73,4 @@ async function run(options) {
 }
 
 
-run(cli.flags);
+run(options);
